e3d95b9669d5d8ba5dbb423e9d2caea9309f329a
miRemid <remidi@126.com>
2019-10-17 20:41:22 +0800

    修改中间件

--- server/middleware.go
+++ server/middleware.go
@@ -7,7 +7,6 @@ import (
 	"context"
 	"io/ioutil"
 	"net/http"
-	"encoding/json"
 	"crypto/sha1"
 	"crypto/hmac"	
 )
@@ -27,27 +26,24 @@ const (
 
 func (bot Bot) pack(middle []CQEventHandler) func(w http.ResponseWriter, r *http.Request){
 	return func(w http.ResponseWriter, r *http.Request){
-		// 将上下文打包成一个event
 		bytedata, _ := ioutil.ReadAll(r.Body)
+		msg := loadintomap(bytedata)
 		defer r.Body.Close()
-		// 封装事件
-		ctx := context.WithValue(r.Context(), WriterKey, w)
-		ctx = context.WithValue(ctx, RequestKey, r)
-		ctx = context.WithValue(ctx, ByteData, bytedata)
-		event := newEvent(ctx)		
-		event.handler = middle
-		event.hlength = len(middle)
-		// 传递中间件
+		ctx := context.WithValue(r.Context(), ByteData, bytedata)
+		event := CQEvent{
+			Type: msg["post_type"].(string),
+			ctx: ctx,
+			writer: w,
+			req: r,
+			handler: middle,
+			hlength: len(middle),
+		}		
 		event.Next()
 	}
 }
 
 // ParseMessage 解析消息转发到不同的Handler中
-func (bot *Bot) ParseMessage(event *CQEvent){
-	bytedata := event.Value(ByteData).([]byte)
-	var msg map[string]interface{}
-	json.Unmarshal(bytedata, &msg)
-	event.Type = msg["post_type"].(string)
+func (bot *Bot) ParseMessage(event CQEvent){
 	switch event.Type {
 	case "message":
 		bot.messageHandler(event)
@@ -58,15 +54,18 @@ func (bot *Bot) ParseMessage(event *CQEvent){
 	case "request":
 		bot.requestHandler(event)
 		break
+	default:
+		break
 	}
 	return
 }
 
-// SignatureMiddleware CQHTTP消息验证中间
-func (bot Bot) signature(event *CQEvent){
+// SignatureMiddleware CQHTTP消息验证中间件
+func (bot Bot) signature(event CQEvent){
 	sig := event.reqHeader().Get("X-Signature")
 	if sig == "" {
 		log.Println("未找到头部信息，请检查CQHTTP配置")
+		event.JSON(204, nil)
 		return
 	}
 	sig = sig[len("sha1="):]
@@ -77,7 +76,8 @@ func (bot Bot) signature(event *CQEvent){
 	res := fmt.Sprintf("%x", mac.Sum(nil))
 	if res != sig {
 		log.Println("消息不来自酷Q，已屏蔽处理")
+		event.JSON(204, nil)
 		return
-	}	
+	}
 	event.Next()
 }
